c66fabf8b8fc01b3c3f044b6d590f230c9864831
wenthemes <wenthemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-01 08:18:14 +0000

    tagging 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/wen-featured-image/tags/1.0.1@1150851 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/partials/wen-featured-image-admin-right-display.php
@@ -0,0 +1,60 @@
+<div class="meta-box-sortables">
+
+    <div class="postbox">
+
+        <h3><span>Help &amp; Support</span></h3>
+        <div class="inside">
+            <h4>Questions, bugs or great ideas?</h4>
+            <p><a href="http://wordpress.org/support/plugin/wen-featured-image" target="_blank">Visit our plugin support page</a></p>
+        </div> <!-- .inside -->
+
+    </div> <!-- .postbox -->
+
+</div> <!-- .meta-box-sortables -->
+
+<div class="meta-box-sortables">
+
+    <div class="postbox">
+
+        <h3><span>Contribute!</span></h3>
+        <div class="inside">
+            <h4>Wanna help make this plugin better?</h4>
+            <ul>
+                <li><a href="http://wordpress.org/support/view/plugin-reviews/wen-featured-image" target="_blank">Review and rate this plugin on WordPress.org</a></li>
+            </ul>
+        </div> <!-- .inside -->
+
+    </div> <!-- .postbox -->
+
+</div> <!-- .meta-box-sortables -->
+
+<div class="meta-box-sortables">
+    <div class="postbox">
+
+        <h3><span>Our Themes and Plugins</span></h3>
+        <div class="inside">
+            <h4>WordPress Themes</h4>
+             <p>
+               <a href="http://wordpress.org/themes/wen-corporate" target="_blank">WEN Corporate</a>
+             </p>
+
+            <h4>WordPress Plugins</h4>
+              <p>
+                <a href="https://wordpress.org/plugins/wen-logo-slider/" target="_blank">WEN Logo Slider</a>
+              </p>
+              <p>
+                <a href="https://wordpress.org/plugins/wen-skill-charts/" target="_blank">WEN Skill Charts</a>
+              </p>
+              <p>
+                <a href="https://wordpress.org/plugins/wen-map-marker/" target="_blank">WEN Map Marker</a>
+              </p>
+              <p>
+                <a href="https://wordpress.org/plugins/wen-responsive-columns/" target="_blank">WEN Responsive Columns</a>
+              </p>
+              <p>
+                <a href="https://wordpress.org/plugins/wen-image-cleaner/" target="_blank">WEN Image Cleaner</a>
+              </p>
+        </div> <!-- .inside -->
+
+    </div> <!-- .postbox -->
+</div> <!-- .meta-box-sortables -->
